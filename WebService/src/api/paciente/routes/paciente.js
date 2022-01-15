ff6abfcd27b58e8dd70f1e77cc2bd2ba320bb708
Amilcar Júnior <amilcarjunior2000@gmail.com>
2022-01-15 17:02:04 -0100

    delete webservice

--- WebService/src/api/paciente/routes/paciente.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * paciente router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::paciente.paciente');
--- WebService/src/api/servico/services/servico.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * servico service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::servico.servico');
